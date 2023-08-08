a8d3216a8761de435d4b44addc0af5ecc54b5c41
moviefan322 <philman202@yahoo.com>
2023-08-08 14:08:44 -0600

    loads cards

--- blackjack.py
+++ blackjack.py
@@ -3,6 +3,24 @@ import tkinter
 
 mainWindow = tkinter.Tk()
 
+
+def load_images(card_images):
+    suits = ["heart", "club", "diamond", "spade"]
+    face_cards = ["jack", "queen", "king"]
+    extension = 'ppm'
+
+    for suit in suits:
+        for card in range(1, 11):
+            name = "cards/{}_{}.{}".format(str(card), suit, extension)
+            image = tkinter.PhotoImage(file=name)
+            card_images.append((card, image,))
+
+        for card in face_cards:
+            name = "cards/{}_{}.{}".format(str(card), suit, extension)
+            image = tkinter.PhotoImage(file=name)
+            card_images.append((10, image,))
+
+
 # Set up screen and frames for dealer and player
 mainWindow.title("Blackjack")
 mainWindow.geometry("640x480")
@@ -28,3 +46,30 @@ tkinter.Label(card_frame, text="Player", background="green",
               fg="white").grid(row=2, column=0)
 tkinter.Label(card_frame, textvariable=player_score_label, background="green",
               fg="white").grid(row=3, column=0)
+
+player_card_frame = tkinter.Frame(card_frame, background="green")
+player_card_frame.grid(row=2, column=1, sticky='ew', rowspan=2)
+
+button_frame = tkinter.Frame(mainWindow)
+button_frame.grid(row=3, column=0, columnspan=3, sticky='w')
+
+dealer_button = tkinter.Button(button_frame, text="Dealer")
+dealer_button.grid(row=0, column=0)
+
+player_button = tkinter.Button(button_frame, text="Player")
+player_button.grid(row=0, column=1)
+
+# load cards
+cards = []
+load_images(cards)
+print(cards)
+
+# create a deck and shuffle cards
+deck = list(cards)
+random.shuffle(deck)
+
+# create list to store dealer and players hands
+dealer_hand = []
+player_hand = []
+
+mainWindow.mainloop()

3deaf34e39ccce8b0a18dfcd52fb24902bbc66c0
moviefan322 <philman202@yahoo.com>
2023-08-10 14:27:28 -0700

    fixes dealing func

--- blackjack.py
+++ blackjack.py
@@ -41,7 +41,22 @@ def score_hand(hand):
 
 
 def deal_dealer():
-    deal_card(dealer_card_frame)
+
+    dealer_score = score_hand(dealer_hand)
+    while 0 < dealer_score < 17:
+        dealer_score_label.set(dealer_score)
+        dealer_hand.append(deal_card(dealer_card_frame))
+        dealer_score = score_hand(dealer_hand)
+
+    player_score = score_hand(player_hand)
+    if player_score > 21:
+        result_text.set("Dealer Wins!")
+    elif dealer_score > 21 or dealer_score < player_score:
+        result_text.set("Player Wins!")
+    elif dealer_score > player_score:
+        result_text.set("Dealer Wins!")
+    else:
+        result_text.set("Draw!")
 
 
 def deal_player():
@@ -122,5 +137,9 @@ random.shuffle(deck)
 # create list to store dealer and players hands
 dealer_hand = []
 player_hand = []
+
+deal_player()
+dealer_hand.append(deal_card(dealer_card_frame))
+deal_player()
 
 mainWindow.mainloop()

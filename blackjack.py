993bea4387d2e43638c8353add7cfd15a9944b9c
moviefan322 <philman202@yahoo.com>
2023-08-10 14:14:29 -0700

    adds logic for player hand

--- blackjack.py
+++ blackjack.py
@@ -1,8 +1,6 @@
 import random
 import tkinter
 
-mainWindow = tkinter.Tk()
-
 
 def load_images(card_images):
     suits = ["heart", "club", "diamond", "spade"]
@@ -22,9 +20,24 @@ def load_images(card_images):
 
 
 def deal_card(frame):
-    next_card = deck.pop()
+    next_card = deck.pop(0)
     tkinter.Label(frame, image=next_card[1], relief='raised').pack(side='left')
     return next_card
+
+
+def score_hand(hand):
+    score = 0
+    ace = False
+    for next_card in hand:
+        card_value = next_card[0]
+        if card_value == 1 and not ace:
+            ace = True
+            card_value = 11
+        score += card_value
+        if score > 21 and ace:
+            score -= 10
+            ace: False
+    return score
 
 
 def deal_dealer():
@@ -32,15 +45,38 @@ def deal_dealer():
 
 
 def deal_player():
-    deal_card(player_card_frame)
+    player_hand.append(deal_card(player_card_frame))
+    player_score = score_hand(player_hand)
+
+    player_score_label.set(player_score)
+    if player_score > 21:
+        result_text.set("Dealer Wins!")
+
+    # global player_score
+    # global player_ace
+    # card_value = deal_card(player_card_frame)[0]
+    # if card_value == 1 and not player_ace:
+    #     player_ace = True
+    #     card_value = 11
+    # player_score += card_value
+    # if player_score > 21 and player_ace:
+    #     player_score -= 10
+    #     player_ace = False
+    # player_score_label.set(player_score)
+    # if player_score > 21:
+    #     result_text.set("Dealer wins")
+    # print(locals())
 
 
+mainWindow = tkinter.Tk()
 # Set up screen and frames for dealer and player
 mainWindow.title("Blackjack")
 mainWindow.geometry("640x480")
+mainWindow.configure(background="green")
 
 result_text = tkinter.StringVar()
 result = tkinter.Label(mainWindow, textvariable=result_text)
+result.grid(row=0, column=0, columnspan=3)
 
 card_frame = tkinter.Frame(mainWindow, relief="sunken",
                            borderwidth=1, background="green")
@@ -56,6 +92,7 @@ dealer_card_frame = tkinter.Frame(card_frame, background="green")
 dealer_card_frame.grid(row=0, column=1, sticky='ew', rowspan=2)
 
 player_score_label = tkinter.IntVar()
+
 tkinter.Label(card_frame, text="Player", background="green",
               fg="white").grid(row=2, column=0)
 tkinter.Label(card_frame, textvariable=player_score_label, background="green",

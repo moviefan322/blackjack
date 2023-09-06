36e8edb561b1313c7da7adc04513209e531a09ac
moviefan322 <philman202@yahoo.com>
2023-09-06 13:41:53 -0400

    adds new game and shuffle buttons

--- blackjack.py
+++ blackjack.py
@@ -21,6 +21,7 @@ def load_images(card_images):
 
 def deal_card(frame):
     next_card = deck.pop(0)
+    deck.append(next_card)
     tkinter.Label(frame, image=next_card[1], relief='raised').pack(side='left')
     return next_card
 
@@ -83,6 +84,36 @@ def deal_player():
     # print(locals())
 
 
+def new_game():
+    global dealer_card_frame
+    global player_card_frame
+    global dealer_hand
+    global player_hand
+    # embedded frame to hold the card images
+    dealer_card_frame.destroy()
+    dealer_card_frame = tkinter.Frame(card_frame, background='green')
+    dealer_card_frame.grid(row=0, column=1, sticky='ew', rowspan=2)
+    # embedded frame to hold the card images
+    player_card_frame.destroy()
+    player_card_frame = tkinter.Frame(card_frame, background="green")
+    player_card_frame.grid(row=2, column=1, sticky='ew', rowspan=2)
+
+    result_text.set("")
+
+    # create list to store dealer and players hands
+    dealer_hand = []
+    player_hand = []
+
+    deal_player()
+    dealer_hand.append(deal_card(dealer_card_frame))
+    dealer_score_label.set(score_hand(dealer_hand))
+    deal_player()
+
+
+def shuffle():
+    random.shuffle(deck)
+
+
 mainWindow = tkinter.Tk()
 # Set up screen and frames for dealer and player
 mainWindow.title("Blackjack")
@@ -119,11 +150,17 @@ player_card_frame.grid(row=2, column=1, sticky='ew', rowspan=2)
 button_frame = tkinter.Frame(mainWindow)
 button_frame.grid(row=3, column=0, columnspan=3, sticky='w')
 
-dealer_button = tkinter.Button(button_frame, text="Dealer", command=deal_dealer)
+dealer_button = tkinter.Button(button_frame, text="Stay", command=deal_dealer)
 dealer_button.grid(row=0, column=0)
 
-player_button = tkinter.Button(button_frame, text="Player", command=deal_player)
+player_button = tkinter.Button(button_frame, text="Hit Me", command=deal_player)
 player_button.grid(row=0, column=1)
+
+new_game_button = tkinter.Button(button_frame, text="New Game", command=new_game)
+new_game_button.grid(row=0, column=2)
+
+shuffle_button = tkinter.Button(button_frame, text="Shuffle", command=shuffle)
+shuffle_button.grid(row=0, column=3)
 
 # load cards
 cards = []
@@ -132,14 +169,12 @@ print(cards)
 
 # create a deck and shuffle cards
 deck = list(cards)
-random.shuffle(deck)
+shuffle()
+
 
 # create list to store dealer and players hands
 dealer_hand = []
 player_hand = []
 
-deal_player()
-dealer_hand.append(deal_card(dealer_card_frame))
-deal_player()
-
+new_game()
 mainWindow.mainloop()
